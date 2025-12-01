a2c1f3bda61c33477e5664e10ed5ebea7e5c09f7
Rodrigo Branas <rodrigo.branas@gmail.com>
2025-12-01 18:44:04 -0300

    updated

--- backend-order/test/Deposit.test.ts
+++ backend-order/test/Deposit.test.ts
@@ -19,7 +19,7 @@ beforeEach(() => {
     deposit = new Deposit();
 });
 
-test.only("Deve fazer um depósito", async () => {
+test("Deve fazer um depósito", async () => {
     const inputSignup = {
         name: "John Doe",
         email: "john.doe@gmail.com",
